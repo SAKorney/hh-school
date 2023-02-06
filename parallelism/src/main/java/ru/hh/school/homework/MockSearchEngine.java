dbb6a6040fbbdf075d3295a8d8e2bf9501d6766d
Korneev Serge <sakorneev90@gmail.com>
2023-02-06 17:50:23 +0300

    Refactoring

--- parallelism/src/main/java/ru/hh/school/homework/MockSearchEngine.java
+++ parallelism/src/main/java/ru/hh/school/homework/MockSearchEngine.java
@@ -1,12 +1,10 @@
 package ru.hh.school.homework;
 
-import java.util.Map;
 import java.util.Random;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class MockSearchEngine {
     private static final Random rnd = new Random();
-    private static final Map<String, Long> SEARCH_CACHE = new ConcurrentHashMap<>();
+
     public static Long mockSearch(String query) {
         try {
             Thread.sleep(1000);
